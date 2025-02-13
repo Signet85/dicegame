3a45f9828492dc21356d21e542e6d12d21dd8a8e
Tenges <tengis_signet@me.com>
2025-02-13 11:54:23 +0800

    toglogchiin eeljiig soligddog bolgoloo

--- app.js
+++ app.js
@@ -22,8 +22,49 @@ document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display="none";
+
+// Шоог шидэх эвэнт листенер
 document.querySelector(".btn-roll").addEventListener("click",function (){
+
+    // 1-6 хүртэл санамсаргүй нэг тоог гаргаж авна
     var diceNumber = Math.floor(Math.random()*6)+1;
+
+    // Шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.style.display="block";
+
+    //Бусаан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ.
     diceDom.src = 'dice-' + diceNumber + '.png';
+
+    //Буусан тоо нь нэгээс ялгаатай бол идэвхтэй тоглогчийн тоог нэмэгдүүлнэ, Тоглогчийн ээлжийн оноог өөрчлнө.
+    if(diceNumber !==1){
+        // 1- с ялгаатай тоо бууоаа.
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }   else {
+        // 1 буусан бол тоглогчийн ээлжийг энэ хэсэгт сольно
+
+        // Энэ тоглогчийн ээлжин дээр цуглуулсан оноог 0 болгоно.
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0 ;
+        // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийн 1 болго.
+
+        // үгүй бол идэвхтэй тоглогчийг 0 болго
+
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0); 
+        //Ингэж бас бичиж болно хураангуй
+
+        // if(activePlayer === 0) {
+        //     activePlayer =1;
+        // }else{
+        //     activePlayer = 0;
+        // }
+        
+        // Улаан цэгийг шижлүүлэх
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //Шоог түр алга болгоно.
+        diceDom.style.display = "none";
+
+    }
 });
